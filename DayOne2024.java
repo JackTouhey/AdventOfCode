6c663e291f717329a221a2d8f7ecb4fe32c96e91
Jack Touhey <jt112112@gmail.com>
2025-06-23 11:42:29 +1200

    Added getInput and sort into 2 lists

--- DayOne2024.java
+++ DayOne2024.java
@@ -1,17 +1,38 @@
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
+import java.util.Scanner;
 
 public class DayOne2024 {
     public static void main(String[] args) {
+        ArrayList<Integer> numbers = getInput();
+        System.out.println("Sorting numbers");
+        ArrayList<Integer> listOne = new ArrayList<>();
+        ArrayList<Integer> listTwo = new ArrayList<>();
+        Boolean odd = true;
+        for (Integer i : numbers){
+            if(odd){
+                listOne.add(i);
+                odd = false;
+            }
+            else{
+                listTwo.add(i);
+                odd = true;
+            }
+        }
+        System.out.println("Printing list one");
+        System.out.println(listOne);
+    }
+    private static ArrayList<Integer> getInput(){
         ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(3);
-        numbers.add(6);
-        numbers.add(1);
-        numbers.add(2);
-        Collections.sort(numbers, new IntegerComparator());
-        System.out.println(numbers);
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            Integer nextNumber = sc.nextInt();
+            System.out.println("Adding " + nextNumber);
+            numbers.add(nextNumber);
+        }
+        sc.close();
+        return numbers;
     }
 }
 class IntegerComparator implements Comparator<Integer> {
