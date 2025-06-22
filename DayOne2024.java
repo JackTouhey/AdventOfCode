7ea1adb4631023ea07dffba7cbfd29c6388c752d
Jack Touhey <jt112112@gmail.com>
2025-06-23 11:06:09 +1200

    Added basic integer comparator

--- DayOne2024.java
+++ DayOne2024.java
@@ -1,5 +1,24 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class DayOne2024 {
     public static void main(String[] args) {
-        System.out.println("We printin");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(6);
+        numbers.add(1);
+        numbers.add(2);
+        Collections.sort(numbers, new IntegerComparator());
+        System.out.println(numbers);
     }
+}
+class IntegerComparator implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o1 - o2;
+    }
+
 }
