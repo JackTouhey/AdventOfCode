769e9380079f32dd611675791069dff6deb3faa0
Jack Touhey <jt112112@gmail.com>
2025-06-24 13:38:13 +1200

    Added Report class and fields

--- DayTwo2024.java
+++ DayTwo2024.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class DayTwo2024{
+    public ArrayList<Report> reports = new ArrayList<>();
     public static void main(String[] args) {
         
+    }
+    private void loadData(){
+
+    }
+}
+class Report {
+    ArrayList<Integer> levels = new ArrayList<>();
+    public Report(ArrayList<Integer> levels){
+        this.levels = levels;
     }
 }
