a628928edb03983b00a69af75d35da1d899a4be7
Jack Touhey <jt112112@gmail.com>
2025-06-25 08:56:05 +1200

    Reached integers in if tree

--- DayThree2024.java
+++ DayThree2024.java
@@ -5,14 +5,49 @@ import java.util.Scanner;
 public class DayThree2024 {
     ArrayList<Mul> validMuls = new ArrayList<>();
     public static void main(String[] args) {
-        
+        loadData();
     }
     public static void loadData(){
         try {
             File dataFile = new File("DataFiles\\DayThreeData.txt");
             Scanner sc = new Scanner(dataFile);
+            sc.useDelimiter("");
+            processData(sc);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+    public static void processData(Scanner sc){
+        while(sc.hasNext()){
+            if(sc.hasNext("m")){
+                System.out.println("In tree: " +  sc.next());
+                if(sc.hasNext("u")){
+                    System.out.println("In tree: " +  sc.next());
+                    if(sc.hasNext("l")){
+                        System.out.println("In tree: " +  sc.next());
+                        if("(".equals(sc.next())){
+                            if(sc.hasNextInt()){
+                                System.out.println("In tree found int: " + sc.nextInt());
+                            }
+                            else{
+                                System.out.println("Moving past: " +  sc.next());
+                            }
+                        }
+                        else{
+                            System.out.println("Moving past: " +  sc.next());
+                        }
+                    }
+                    else{
+                        System.out.println("Moving past: " +  sc.next());
+                    }
+                }
+                else{
+                    System.out.println("Moving past: " +  sc.next());
+                }
+            }
+            else{
+                System.out.println("Moving past: " +  sc.next());
+            }
         }
     }
 }
