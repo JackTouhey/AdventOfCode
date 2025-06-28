1d3ba2b321ae585b3e0ff58ce79cb8c8f5624396
Jack Touhey <jt112112@gmail.com>
2025-06-28 12:49:01 +1200

    Created Rule class

---
+++ DayFive2024.java
@@ -0,0 +1,17 @@
+public class DayFive2024 {
+    
+}
+class Rule{
+    Integer first;
+    Integer last;
+    public Rule(Integer first, Integer last){
+        this.first = first;
+        this.last = last;
+    }
+    public Integer getFirst(){
+        return first;
+    }
+    public Integer getLast(){
+        return last;
+    }
+}
