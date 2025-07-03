7fd391a1c9610d7b0728f904844c81f81b10c91e
Jack Touhey <jt112112@gmail.com>
2025-07-03 14:57:20 +1200

    Now loading data

--- DaySix2024.java
+++ DaySix2024.java
@@ -1,3 +1,52 @@
+import java.io.*;
+import java.util.*;
 public class DaySix2024 {
-    
+    public static void main(String[] args) {
+        String[][] grid = loadData(); 
+        for(int y = 0; y < grid.length; y++){
+            for(int x = 0; x < grid[y].length; x++){
+                System.out.print(grid[y][x]);
+            }
+            System.out.println();
+        }
+    }
+    public static String[][] loadData(){
+        String[][] returnGrid = new String[0][0];
+        try {
+            File dataFile= new File("DataFiles\\DaySixTestData.txt");
+            Scanner sc = new Scanner(dataFile);
+            sc.useDelimiter("");
+            String[][] grid = createArray(sc);
+            Scanner sc2 = new Scanner(dataFile);
+            sc2.useDelimiter("");
+            returnGrid = populateArray(sc2, grid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return returnGrid;
+    }
+    public static String[][] createArray(Scanner sc){
+        int height = 1;
+        String rowOne = sc.nextLine();
+        int width = rowOne.length();
+        while(sc.hasNext()){
+            height++;
+            sc.nextLine();
+        }
+        System.out.println("Array height: " + height + " Array width: " + width);
+        String[][] grid = new String[height][width];
+        return grid;
+    }
+    public static String[][] populateArray(Scanner sc, String[][] grid){
+        for(int i = 0; i < grid.length; i++){
+            Scanner sc2 = new Scanner(sc.nextLine());
+            for(int ii = 0; ii < grid[0].length; ii++){
+                sc2.useDelimiter("");
+                String next = sc2.next();
+                // System.out.println("Column: " + i +" Row: " + ii + " Adding: " + next);
+                grid[i][ii] = next;
+            }
+        }
+        return grid;
+    }
 }
