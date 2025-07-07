9728700b50f3c384a1e9baa406adc9dd86e33908
Jack Touhey <jt112112@gmail.com>
2025-07-07 13:38:37 +1200

    Set up Equation class

--- DaySeven2024.java
+++ DaySeven2024.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class DaySeven2024 {
-    
+    public static void main(String[] args) {
+        
+    }
+}
+class Equation{
+    Integer testValue;
+    ArrayList<Integer> values = new ArrayList<>();
+    public Equation(Integer testValue, ArrayList<Integer> values){
+        this.testValue = testValue;
+        this.values = new ArrayList<>(values);
+    }
 }
