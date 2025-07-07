3f929217bb69aaabfc97faa53ed2636bb03aca06
Jack Touhey <jt112112@gmail.com>
2025-07-08 09:44:16 +1200

    Added antenna model class and data loading methods

--- DayEight2024.java
+++ DayEight2024.java
@@ -1,5 +1,71 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class DayEight2024 {
+    private static String[][] grid = loadData();
+    private static ArrayList<Antenna> antennae = new ArrayList<>();
+    private static HashMap<Character, ArrayList<Antenna>> antennaByFrequency = new HashMap<>();
     public static void main(String[] args) {
-        
+        printGrid(grid);
+    }
+    public static void printGrid(String[][] grid){
+        for(int y = 0; y < grid.length; y++){
+            for(int x = 0; x < grid[y].length; x++){
+                System.out.print(grid[y][x]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static String[][] loadData(){
+        String[][] returnGrid = new String[0][0];
+        try {
+            File dataFile= new File("DataFiles\\DayEightTestData.txt");
+            Scanner sc = new Scanner(dataFile);
+            sc.useDelimiter("");
+            String[][] grid = createArray(sc);
+            Scanner sc2 = new Scanner(dataFile);
+            sc2.useDelimiter("");
+            returnGrid = populateArray(sc2, grid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return returnGrid;
+    }
+    public static String[][] createArray(Scanner sc){
+        int height = 1;
+        String rowOne = sc.nextLine();
+        int width = rowOne.length();
+        while(sc.hasNext()){
+            height++;
+            sc.nextLine();
+        }
+        System.out.println("Array height: " + height + " Array width: " + width);
+        String[][] grid = new String[height][width];
+        return grid;
+    }
+    public static String[][] populateArray(Scanner sc, String[][] grid){
+        for(int i = 0; i < grid.length; i++){
+            Scanner sc2 = new Scanner(sc.nextLine());
+            for(int ii = 0; ii < grid[0].length; ii++){
+                sc2.useDelimiter("");
+                String next = sc2.next();
+                System.out.println("Column: " + i +" Row: " + ii + " Adding: " + next);
+                grid[i][ii] = next;
+            }
+        }
+        return grid;
+    }
+}
+class Antenna{
+    int antennaX;
+    int antennaY;
+    char frequency;
+    public Antenna(int antennaX, int antennaY, char frequency){
+        this.antennaX = antennaX;
+        this.antennaY = antennaY;
+        this.frequency = frequency;
     }
 }
