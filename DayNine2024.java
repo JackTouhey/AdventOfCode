7ff1a12a8f5f9b836393fbf5b22fe7237b055116
Jack Touhey <jt112112@gmail.com>
2025-07-10 13:20:22 +1200

    Added model class FileBlock

--- DayNine2024.java
+++ DayNine2024.java
@@ -1,3 +1,16 @@
 public class DayNine2024 {
     
 }
+class FileBlock{
+    private final int ID;
+    private final int size;
+    private final int followingFreeSpace;
+    public FileBlock(int ID, int size, int followingFreeSpace){
+        this.ID = ID;
+        this.size = size;
+        this.followingFreeSpace = followingFreeSpace;
+    }
+    public int getID(){return this.ID;}
+    public int getSize(){return this.size;}
+    public int getFollowingFreeSpace(){return this.followingFreeSpace;}
+}
