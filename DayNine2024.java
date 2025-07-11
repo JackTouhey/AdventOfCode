b9441ccc46e1a6b3ac2b226fc1a36f09af509ed7
Jack Touhey <jt112112@gmail.com>
2025-07-11 14:47:49 +1200

    Added print statements for debugging

--- DayNine2024.java
+++ DayNine2024.java
@@ -53,27 +53,42 @@ public class DayNine2024 {
         }
     }
     private static void sortWholeBlocks(ArrayList<String> inputBlock){
+        //Moves down the block right to left
         for(int i = inputBlock.size()-1; i >= 0; i--){
+            //Checks if current position is a block
             if(!inputBlock.get(i).equals(".")){
                 FileBlock currentBlock = getFileBlockByID(Integer.parseInt(inputBlock.get(i)));
+                System.out.println("Found block to move left. ID: " + currentBlock.getID() + " size: " + currentBlock.getSize() + " index: " + i);
                 int blockSize = currentBlock.getSize();
                 int startOfFreeSpace = 0;
                 int freeSpaceIndex = 0;
                 int freeSpaceSize = 0;
-                while(freeSpaceSize < blockSize && freeSpaceIndex < inputBlock.size()){
+                //Finds leftmost free space that can accommodate current block
+                while(freeSpaceSize < blockSize && freeSpaceIndex < inputBlock.size()-1){
                     freeSpaceSize = 0;
+                    System.out.println("In while loop going left to right to find free space. freeSpaceIndex: " + freeSpaceIndex);
                     if(inputBlock.get(freeSpaceIndex).equals(".")){
                         startOfFreeSpace = freeSpaceIndex;
                         while(inputBlock.get(freeSpaceIndex).equals(".")){
+                            System.out.println("Found free space at index: " + startOfFreeSpace + " current size: " + freeSpaceSize + " at index: " + freeSpaceIndex);
                             freeSpaceIndex++;
                             freeSpaceSize++;
                         }
                     }
+                    else{
+                        freeSpaceIndex++;
+                    }
                 }
+                //If free space found, move the block in
                 if(freeSpaceSize >= blockSize){
+                    System.out.println("Moving block w/ ID: " + currentBlock.getID() + " to free space starting: " + startOfFreeSpace + " array before swap: " + inputBlock);
                      for(int ii = 0; ii < currentBlock.getSize(); ii++) {
                         Collections.swap(inputBlock, startOfFreeSpace + ii, i - ii);
                     }
+                    System.out.println("Array after swap: " + inputBlock);
+                }
+                else{
+                    System.out.println("Unable to find free space for block ID: " + currentBlock.getID());
                 }
             }
         }
