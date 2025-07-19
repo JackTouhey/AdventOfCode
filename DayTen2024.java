eaf33ab9f66211ced304657cb411c588106c9347
Jack Touhey <jt112112@gmail.com>
2025-07-19 11:59:43 +1000

    Added grid generator class to remove duplicate code going forward

--- DayTen2024.java
+++ DayTen2024.java
@@ -1,9 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class DayTen2024 {
-    private static final Integer[][] topographicMap = loadData();
+    private static final Integer[][] topographicMap = GridGenerator.generateIntegerGrid("DataFiles\\DayTenTestData.txt");
     public static void main(String[] args) {
         for(Integer[] row : topographicMap){
             for(Integer i : row){
@@ -11,43 +8,5 @@ public class DayTen2024 {
             }
             System.out.println();
         }
-    }
-    public static Integer[][] loadData(){
-        Integer[][] returnGrid = new Integer[0][0];
-        try {
-            File dataFile= new File("DataFiles\\DayTenTestData.txt");
-            Scanner sc = new Scanner(dataFile);
-            sc.useDelimiter("");
-            Integer[][] grid = createArray(sc);
-            Scanner sc2 = new Scanner(dataFile);
-            sc2.useDelimiter("");
-            returnGrid = populateArray(sc2, grid);
-        } catch (FileNotFoundException e) {
-        }
-        return returnGrid;
-    }
-    public static Integer[][] createArray(Scanner sc){
-        int height = 1;
-        String rowOne = sc.nextLine();
-        int width = rowOne.length();
-        while(sc.hasNext()){
-            height++;
-            sc.nextLine();
-        }
-        System.out.println("Array height: " + height + " Array width: " + width);
-        Integer[][] grid = new Integer[height][width];
-        return grid;
-    }
-    public static Integer[][] populateArray(Scanner sc, Integer[][] grid){
-        for (Integer[] row : grid) {
-            try (Scanner sc2 = new Scanner(sc.nextLine())) {
-                for (int ii = 0; ii < row.length; ii++) {
-                    sc2.useDelimiter("");
-                    Integer next = sc2.nextInt();
-                    row[ii] = next;
-                }
-            } catch (Exception e) {}
-        }
-        return grid;
     }
 }
