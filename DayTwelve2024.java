d080cc5e683caf7ff7cbd71db500a73c988c59ad
Jack Touhey <jt112112@gmail.com>
2025-07-21 15:25:13 +1000

    Setup basic Region model class

--- DayTwelve2024.java
+++ DayTwelve2024.java
@@ -1,11 +1,25 @@
+
+import java.util.HashSet;
+
 public class DayTwelve2024 {
     private static final char[][] garden = DataLoader.generateCharGrid("DataFiles\\DayTwelveTestData.txt");
     public static void main(String[] args) {
+        printGarden();
+    }
+    private static void printGarden(){
         for(char[] row : garden){
             for(char c : row){
                 System.out.print(c);
             }
             System.out.println();
         }
+    }
+}
+class Region{
+    private final HashSet<Coordinate> plots;
+    private final char plant;
+    public Region(HashSet<Coordinate> plots, char plant){
+        this.plots = plots;
+        this.plant = plant;
     }
 }
